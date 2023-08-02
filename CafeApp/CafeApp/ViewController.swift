d6631c274876fda09f4ff2fef4b2c2a7ac10af90
Thierno Diallo <tdiallo1996@gmail.com>
2023-08-01 23:43:45 -0400

    Added the Xcode File

---
+++ CafeApp/CafeApp/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  CafeApp
+//
+//  Created by Thierno Diallo on 8/1/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+        
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .systemRed
+        
+
+    }
+
+
+}
+
